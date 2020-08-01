548f7c386f3d1b47eb5442b77c23b2556b5da5b0
Alyssa <59900390+alyssahyq@users.noreply.github.com>
2020-08-01 16:34:02 -0400

    Update bc.py

--- bc.py
+++ bc.py
@@ -1,8 +1,17 @@
+import os
+import subprocess
 import gramfuzz
 
-fuzzer = gramfuzz.GramFuzzer()
-fuzzer.load_grammar("./bc_grammar.py")
-bc_inputs = fuzzer.gen(cat="bc_input", num=10)
-#should dump them into a file
-for bc_input in bc_inputs:
-    print(bc_input.decode("utf-8"))
+def generate_input():
+    fuzzer = gramfuzz.GramFuzzer()
+    fuzzer.load_grammar("./bc_grammar.py")
+    bc_inputs = fuzzer.gen(cat="bc_input", num=10)
+    with open('input', 'w') as f:
+        for bc_input in bc_inputs:
+            f.write(bc_input.decode('utf-8') + '\n')
+        f.write('quit')
+
+if __name__ == "__main__":
+    generate_input()
+    result = subprocess.check_output(['bc', 'input'], stderr=subprocess.STDOUT).decode('utf-8')
+    print(result)

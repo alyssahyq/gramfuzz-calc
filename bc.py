369efbed85711417959296114db31ca341d6efc2
evan_ysj <54757599+evan-ysj@users.noreply.github.com>
2020-08-07 21:23:54 -0400

    update bc.py

--- bc.py
+++ bc.py
@@ -1,3 +1,11 @@
+'''
+@Title: 
+@Prject: 
+@Description: 
+@Author: Sujia Yin
+@Date: 2020-07-27 16:31:34
+@Version: 1.0
+'''
 import os
 import subprocess
 import gramfuzz
@@ -5,13 +13,34 @@ import gramfuzz
 def generate_input():
     fuzzer = gramfuzz.GramFuzzer()
     fuzzer.load_grammar("./bc_grammar.py")
-    bc_inputs = fuzzer.gen(cat="bc_input", num=10)
-    with open('input', 'w') as f:
-        for bc_input in bc_inputs:
-            f.write(bc_input.decode('utf-8') + '\n')
-        f.write('quit')
+    for i in range(1000):
+        bc_inputs = fuzzer.gen(cat="bc_input", num=200)
+        with open('input{}'.format(i), 'w') as f:
+            for bc_input in bc_inputs:
+                f.write(bc_input.decode('utf-8') + '\n')
+            f.write('quit')
+
 
 if __name__ == "__main__":
     generate_input()
-    result = subprocess.check_output(['bc', 'input'], stderr=subprocess.STDOUT).decode('utf-8')
-    print(result)
+    result = None
+    try:
+        #result = subprocess.check_output(['python', 'except.py'], stderr=subprocess.STDOUT).decode('utf-8')
+        result = subprocess.check_output(['bc', '-l', 'input*'], stderr=subprocess.STDOUT).decode('utf-8')
+    except Exception as e:
+        result = 'Exception: {}'.format(e);
+    finally: 
+        runtime_error = 0
+        exception = 0
+        test_num = 0
+        res = result.split('\n')
+        for line in res:
+            test_num += 1
+            if 'error' in line:
+                runtime_error += 1
+            if 'Exception' in line:
+                print(line)
+                exception += 1
+        print('tested:', test_num, 'error:', runtime_error, 'exception:', exception)
+    
+
---
+++ except.py
@@ -0,0 +1,11 @@
+'''
+@Title: 
+@Prject: 
+@Description: 
+@Author: Sujia Yin
+@Date: 2020-08-05 17:53:32
+@Version: 1.0
+'''
+
+x = [1 for _ in range(10)]
+print(x[10])

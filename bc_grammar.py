b6a2e416cc54edab935d28ad1712e50f955f89ca
Alyssa <59900390+alyssahyq@users.noreply.github.com>
2020-08-11 21:32:31 -0400

    Added grammar for sqrt

--- bc_grammar.py
+++ bc_grammar.py
@@ -31,18 +31,18 @@ NDef("assignment_operators", Or('='))
 #arith = arithmetic
 #repeating patterns for arithmetic operation
 arith = Join(NRef("arithmetic_operator"),NRef("int"), sep=" ")
-arith_patterns = Join(arith, max=200, sep=" ")
+arith_patterns = Join(arith, max=20, sep=" ")
 #adding parenthesis for rithmetic operation
 arith_paren = Join("(",NRef("int"),arith_patterns,")", sep=" ")
 arith_number_paren = Join(NRef("arithmetic_operator"),Or(arith_paren,NRef("int")), sep=" ")
-arith_number_paren_patterns = Join(arith_number_paren, max=200, sep=" ")
-
+arith_number_paren_patterns = Join(arith_number_paren, max=20, sep=" ")
+sqrt = Join("sqrt",arith_paren,sep = '')
 #stack to process parenth
 Def("bc_input",
     Join(
         Or(arith_paren,NRef("int")),
         arith_number_paren_patterns,
-        Opt(Join(NRef("relational_operator"),Or(arith_paren,NRef("int")), sep=" ")),
+        Opt(Join(NRef("relational_operator"),Or(arith_paren,NRef("int"),sqrt), sep=" ")),
         arith_number_paren_patterns,
     sep=" "),
     cat="bc_input"

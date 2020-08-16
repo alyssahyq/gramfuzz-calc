78562448cdccc15fe581cc91a017f8962a10b493
Alyssa <59900390+alyssahyq@users.noreply.github.com>
2020-08-16 15:30:48 -0400

    Simplify the grammar

--- statement_grammar.py
+++ statement_grammar.py
@@ -22,61 +22,63 @@ array_i = Join(variable_name,'[',array_ind,']',sep='')
 var_post = Join(Or(variable_name,variable_name,variable_name,array_i),Or('++','--'),sep='')
 var_pre = Join(Or('++','--'),Or(variable_name,variable_name,variable_name,array_i),sep='')
 variable_list = Or(array,array,variable_name,variable_name,variable_name,array_i,var_post,var_pre)
-call_function = Join(function_name,'(',variable_list')',sep='')
+para_list = Join(variable_list,Opt(Join(Join(',',variable_list,sep=''),max=statement_max ,sep=' ')),sep=' ')
+call_function = Join(function_name,'(',para_list,')',sep='')
 variable_odd =  Or(array,array,variable_name,variable_name,variable_name,array_i,call_function,call_function,call_function,
                    var_post,var_pre)
 
 assign_operation = Or('=','+=','-=','*=','/=','^=','%=')
 arith_operation = Or('+','-','*','/','%','^')
 arith_expr = Join(Or(variable_odd,NRef('int')),
-                  Join(Join(arith_operation,Or(variable_odd,NRef('int')),sep='')
-                      ,max=statement_max,sep='')
+                  Opt(Join(Join(arith_operation,Or(variable_odd,NRef('int')),sep='')
+                      ,max=statement_max,sep=''))
                   ,sep='')
-assign = Or(Join(variable_odd,assign_operation,Or(variable_odd,NRef('int')),sep=' '))
+assign = Join(variable_odd,assign_operation,arith_expr,sep=' ')
 
 expr_operation = Or('>','<','>=','<=','==','!=')
 condition = Join(arith_expr,expr_operation,arith_expr,sep=' ')
 
 auto = Join('auto',variable_odd,Opt(Join(',',variable_odd,sep=' ')),sep=' ')
 
+statement = Join(Or(auto,assign,assign,assign,call_function,call_function),max=statement_max,sep='\n')
+
+
 if_else = Join('if (',condition,'){','\n',
-               Join(Or(auto,assign,assign,assign,assign,assign),max=statement_max,sep='\n'),'}','\n',
+               statement,'}','\n',
                Opt(Join('else{','\n',
-                        Join(Or(auto,assign,assign,assign,assign,assign),max=statement_max,sep='\n'),'}',sep='')),
+                        statement,'}',sep='')),
                sep=''
 )
 
 for_loop = Join('for(',Or(assign,variable_odd),';',condition,';',assign,'){','\n',
                 Join(Or(auto,assign),max=5,sep='\n'),'\n',
                 Opt(Join('if (',condition,'){','\n',
-                         Join(Or(auto,assign,assign,assign,assign,assign,assign,'break','continue'),max=statement_max,sep='\n'),'}','\n',
+                         Join(Or(auto,assign,assign,assign,call_function,call_function,'break','continue')
+                              ,max=statement_max,sep='\n'),'}','\n',
                          Opt(Join('else{','\n',
-                                  Join(Or(auto,assign,'break','continue'),max=statement_max,sep='\n'),'}',sep='')),sep='')),
+                                  Join(Or(auto,assign,assign,assign,call_function,call_function,'break','continue')
+                                       ,max=statement_max,sep='\n'),'}',sep='')),sep='')),
                 '}'
                 ,sep='')
 
 while_loop = Join('while(',condition,'){','\n',
                 Join(Or(auto,assign,assign,assign,assign,assign,assign),'\n',max=statement_max,sep='\n'),'\n',
                 Opt(Join('if (',condition,'){','\n',
-                         Join(Or(auto,assign,assign,assign,assign,assign,'break','continue'),max=statement_max,sep='\n'),'}','\n',
+                         Join(Or(auto,assign,assign,assign,call_function,call_function,'break','continue')
+                              ,max=statement_max,sep='\n'),'}','\n',
                          Opt(Join('else{','\n',
-                                  Join(Or(auto,assign,assign,assign,assign,assign,assign,'break','continue'),max=statement_max,sep='\n'),'}',sep='')),sep='')),
+                                  Join(Or(auto,assign,assign,assign,call_function,call_function,'break','continue')
+                                       ,max=statement_max,sep='\n'),'}',sep='')),sep='')),
                 '}'
                 ,sep='')
 
-variable_list = Join(variable_odd,
-                     Opt(Join(
-                         Join(',',variable_odd,sep='')
-                         ,max=statement_max,sep=''))
-                     ,sep='')
-
 #max in def_fun: in
-def_fun = Join('define ',function_name,'(',variable_list,'){','\n',
-               Join(Or(while_loop,assign,assign,assign,assign,assign,for_loop,auto,if_else),max=statement_max,sep='\n'),
+def_fun = Join('define ',function_name,'(',para_list,'){','\n',
+               Join(Or(while_loop,assign,assign,assign,assign,assign,for_loop,auto,if_else),max=statement_max,sep='\n'),'\n',
                Opt(Join('return ',Or(variable_odd,NRef('int')),'\n',sep='')),'}'
                ,sep='')
 
 Def("bc_input",
-    Join(Or(def_fun,while_loop,assign,for_loop,auto,if_else),max=statement_max,sep="\n"),
+    Join(Or(def_fun,while_loop,assign,call_function,for_loop,auto,if_else),max=statement_max,sep="\n"),
     cat="bc_input"
 )

88376be3f98557a37ebe3228b1ab27e1652daa1f
Alyssa <59900390+alyssahyq@users.noreply.github.com>
2020-08-17 23:14:04 -0400

    Add int limit

--- grammar/arith_grammar.py
+++ grammar/arith_grammar.py
@@ -39,7 +39,7 @@ class NRef(Ref):
 class NDef(Def):
     cat = "name_def"
 #134217729 is the biggest int that will not trigger "error: exponent too large in raise"
-NDef("int", Int(odds = [(0.000000000000001,[0]),(0.90,[-134217729,134217729])]))
+NDef("int", Int(odds = [(0.000000000000001,[0]),(0.99,[-134217729,134217729])]))
 NDef("float",Float(odds = [(0.000000000000001,[0]),(0.85,[-100,100])])))
 NDef("arithmetic_operator",Or('+','-','*','%','/','^'))
 NDef("relational_operator",Or('<','<=','>','>=','==','!='))

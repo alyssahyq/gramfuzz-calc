d84ed1aa3e68d6f48f7fd3d2e753c38e03ae9711
evan_ysj <54757599+evan-ysj@users.noreply.github.com>
2020-08-20 22:44:13 -0400

    Add files via upload

--- generator.py
+++ generator.py
@@ -8,6 +8,7 @@ Version: 1.0
 '''
 import abc
 import gramfuzz
+import random
 from pyZZUF.pyZZUF import pyZZUF
 
 
@@ -40,23 +41,24 @@ class MutateGenerator(Generator):
         print('\nGenerating seeds...')
         f = open(self._feed)
         seed_input = f.read()
-        seed_byte = seed_input.encode('utf-8')
-        zzuf = pyZZUF(seed_byte)
         f.close()
-        zzuf.set_protected('quit', True)
-        zzuf.set_protected('if', True)
-        zzuf.set_protected('else', True)
-        zzuf.set_protected('define', True)
-        zzuf.set_protected('auto', True)
-        zzuf.set_protected('local', True)
-        zzuf.set_protected('{', True)
-        zzuf.set_protected('}', True)
         for i in range(self._file_num):
-            zzuf_array =zzuf.mutate()
-            bc_input=''
+            seed_byte = seed_input.encode('utf-8')
+            zzuf = pyZZUF(seed_byte, random.randint(0, 100), random.uniform(0.004, 0.1), None)
+            zzuf.set_protected('quit', True)
+            zzuf.set_protected('if', True)
+            zzuf.set_protected('else', True)
+            zzuf.set_protected('define', True)
+            zzuf.set_protected('auto', True)
+            zzuf.set_protected('local', True)
+            zzuf.set_protected('{', True)
+            zzuf.set_protected('}', True)
+            zzuf_array = zzuf.mutate()
+            bc_input = ''
             for j in range(len(zzuf_array)):
-                bc_input=bc_input+chr(zzuf_array[j])
+                bc_input += chr(zzuf_array[j])
             with open('input/input{}'.format(i), 'w') as f:
                 f.write(bc_input)
-                f.write('quit')
         print('{} files generated. Start fuzzing...'.format(self._file_num))
+        
+        
